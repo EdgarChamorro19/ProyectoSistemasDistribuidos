f79638885fb20696c1f6d55b8efe98bd89b35f0c
Edgar Chamorro Astudillo <edfabrizio05@gmail.com>
2022-08-28 22:19:15 -0500

    Recien Subido

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Inicio(
+      ),
+    );
+  }
+}
+
+class Inicio extends StatefulWidget {
+  Inicio({Key? key}) : super(key: key);
+
+  @override
+  State<Inicio> createState() => _InicioState();
+}
+
+class _InicioState extends State<Inicio> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        minimum: const EdgeInsets.symmetric(vertical: 10),
+        child: Column(
+          children: <Widget> [
+            Flexible(
+              child: Container(
+       
+                child: Image.asset("imagenes/imginicio.png",height: 350,),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 30,vertical: 10),
+              child: Text("Bienvenidos a Nuestra Aplicacion",
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize:30)
+                      ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 30,vertical: 15),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(340, 55),
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(13))
+                  ),
+                ),
+                onPressed: (){
+
+                },
+                child: Text("Jugar",style: TextStyle(fontWeight: FontWeight.bold, fontSize:15)),
+              ),
+            ),
+          ],
+        )
+      )
+    );
+  }
+}
