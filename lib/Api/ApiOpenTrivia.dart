bb6c368c876ca0e4a788e2309d1206eecbf29abc
Edgar Chamorro Astudillo <edfabrizio05@gmail.com>
2022-08-28 23:44:50 -0500

    Actualizado - Api

---
+++ lib/Api/ApiOpenTrivia.dart
@@ -0,0 +1,30 @@
+import 'dart:developer';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:trivia_app/Model/Preguntas.dart';
+
+class ApiTrivia {
+  Future<List<Results>?> getStates() async {
+    Uri url = Uri.parse("https://opentdb.com/api.php?amount=10&category=21&difficulty=hard");
+
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      log('api worked ${response.body}');
+      var body = response.body;
+      var statesJsonArray = json.decode(body)['results'];
+
+      try {
+        List<Results> results =
+            (statesJsonArray as List).map((e) => Results.fromJson(e)).toList();
+
+        return results;
+      } catch (e) {
+        log('try failed $e');
+      }
+    } else {
+      log('api request failed ${response.body}');
+
+      return null;
+    }
+  }
+}
